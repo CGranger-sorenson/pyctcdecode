bce72ce3de85c8448fb1da40ec8e3180c62a5a5e
Chris Granger <124754883+CGranger-sorenson@users.noreply.github.com>
2024-10-18 11:59:17 -0700

    Fix bug where beam does not update partial frames on repeated characters

--- pyctcdecode/decoder.py
+++ pyctcdecode/decoder.py
@@ -462,7 +462,7 @@ class BeamSearchDecoderCTC:
                     # if only blank token or same token
                     if char == "" or beam.last_char == char:
                         if char == "":
-                            new_end_frame = beam.partial_frames[0]
+                            new_end_frame = beam.partial_frames[1]
                         else:
                             new_end_frame = frame_idx + 1
                         new_part_frames = (
